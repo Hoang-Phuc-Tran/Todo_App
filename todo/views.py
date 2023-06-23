64b80df9102ec9aa52b47bc9c89b86927436e082
ptran9102 <hoangphuctran9102@gmail.com>
2023-06-23 00:41:41 -0400

    mark as undone

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
 from django.http import HttpResponse
+from .models import Task
+
 # Create your views here.
 def addTask(request):
-    return HttpResponse('the form is submitted')
-    
+    task = request.POST['task']
+    Task.objects.create(task=task)
+    return redirect('home')
+
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+
+def deleteTask(request,pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('home')
